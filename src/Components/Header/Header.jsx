cc5595e841ae5456ababb566d1007fc10459ee1d
tonmoy00saha <tonmoy.official.20@gmail.com>
2024-08-28 17:48:24 +0600

    navbar design complete

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,52 @@
+import { NavLink } from "react-router-dom";
+import './Header.css'
+
+const Header = () => {
+    const Links =
+    <>
+    <li><NavLink to="/">Home</NavLink></li>
+    <li><NavLink to="/listedbook">Listed Books</NavLink></li>
+    <li><NavLink to="/pagestoread">Pages to Read</NavLink></li>
+    </>
+    return (
+        <div className="navbar bg-base-100 worksans">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor">
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16" />
+              </svg>
+            </div>
+            <ul
+              tabIndex={0}
+              className=" menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow menu-container ">
+              {Links}
+            </ul>
+          </div>
+          <a className=" text-3xl font-bold">Book Vibe</a>
+        </div>
+        <div className="navbar-center hidden lg:flex">
+          <ul className="flex text-lg text-[#131313CC] menu-container ">
+          {Links}
+          </ul>
+        </div>
+        <div className="navbar-end">
+         <div className="flex gap-4 ">
+         <a className="btn bg-primary text-white">Sign in</a>
+         <a className="btn bg-[#59C6D2] text-white">Sign up</a>
+         </div>
+        </div>
+      </div>
+    );
+};
+
+export default Header;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,28 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
 
+import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Root from './Components/Root/Root.jsx';
+
+import Home from './Components/Home/Home.jsx';
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root></Root>,
+    children:[
+      {
+        path: "/",
+        element: <Home></Home>,
+      }
+    ],
+  },
+]);
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </StrictMode>,
 )
