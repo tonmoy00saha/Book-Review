371fb97eeb8aecd06d76d16648323fd8d1fa9407
tonmoy00saha <tonmoy.official.20@gmail.com>
2024-08-30 22:21:17 +0600

    link updated

--- src/Components/Book/Book.jsx
+++ src/Components/Book/Book.jsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 const Book = ({ book }) => {
     const {image, bookName, tags, author, category, rating, bookId} = book;
     return (
-        <Link to={`./book/${bookId}`}>
+        <Link to={`/book/${bookId}`}>
         <div className="card bg-base-100 shadow-xl space-y-4 ">
             <figure className="px-10 pt-10 ">
                 <img
